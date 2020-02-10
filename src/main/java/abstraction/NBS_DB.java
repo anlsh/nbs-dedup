cbb4e5cd07ed1cde1e50f2b84120bfae82bea818
Anish <anlsh@protonmail.com>
2020-02-10 17:33:51 -0500

    Add function to read entire database into HashMap

--- src/main/java/abstraction/NBS_DB.java
+++ src/main/java/abstraction/NBS_DB.java
@@ -1,11 +1,10 @@
 package abstraction;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class NBS_DB {
 
@@ -17,11 +16,30 @@ public class NBS_DB {
 
         conn = DriverManager.getConnection(connectionUrl);
     }
-//    public List<String> get_id_list() throws SQLException {
-//
-//        Statement query = conn.createStatement();
-//        String queryText = "SELECT TOP 10 * FROM Person.Contact"
-//    }
+    public List<Map<String, Object>> getDatabaseAsMap() throws SQLException {
+
+        // TODO might want to convert this to an array later on... fine for now though
+        List ll = new LinkedList<Map<String, Object>>();
+
+        Statement query = conn.createStatement();
+        ResultSet rs = query.executeQuery("SELECT * from Person");
+        ResultSetMetaData rsMeta = rs.getMetaData();
+
+        int num_columns = rsMeta.getColumnCount();
+
+        // TODO This methodology is sourced from https://stackoverflow.com/questions/7507121/efficient-way-to-handle-resultset-in-java
+        // But should be abstracted using a standard library like DBUtils or MapListHandler
+        while (rs.next()) {
+            HashMap row = new HashMap<String, Object>(num_columns);
+
+            for (int c = 1; c <= num_columns; c++) {
+                row.put(rsMeta.getColumnName(c), rs.getObject(c));
+            }
+            ll.add(row);
+        }
+
+        return ll;
+    }
 //    public Object get_id_attr(DatabaseRecord id, RecordFields field) {
 //        // TODO Implement
 //        // Get database[id][field]
--- src/test/java/abstraction/NBS_DBTest.java
+++ src/test/java/abstraction/NBS_DBTest.java
@@ -1,15 +1,22 @@
 package abstraction;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.sql.SQLException;
 
-import static org.junit.Assert.*;
-
 public class NBS_DBTest {
-    @Test
-    public void databaseConnectedSucceeds() throws SQLException {
-        NBS_DB db = new NBS_DB("localhost", 1433, "ODS_PRIMARY_DATA01",
+
+    private NBS_DB db;
+
+    @Before
+    public void setupDatabaseConnection() throws SQLException {
+        db = new NBS_DB("localhost", 1433, "ODS_PRIMARY_DATA01",
                 "SA", "saYyWbfZT5ni7t");
+    }
+
+    @Test
+    public void testGetPersonIDs() throws SQLException {
+        db.getDatabaseAsMap();
     }
 }
