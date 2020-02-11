47bfc59b40dbf86819283c4ad105b2d610ed0f9a
Anish <anlsh@protonmail.com>
2020-02-11 15:51:47 -0500

    Reformat Winston's awful style

--- src/main/java/abstraction/AuxMap.java
+++ src/main/java/abstraction/AuxMap.java
@@ -1,11 +1,10 @@
 package abstraction;
 
 import java.io.*;
-import java.sql.SQLException;
 import java.util.*;
 
 public class AuxMap { //manages the aux maps
-    public static boolean auxTableExists(final Set<MatchFieldEnum> attrs){
+    public static boolean auxTableExists(final Set<MatchFieldEnum> attrs) {
         List<MatchFieldEnum> attrList = new ArrayList<MatchFieldEnum>(attrs);
         Collections.sort(attrList);
         String attributes_str = "";
@@ -21,18 +20,16 @@ public class AuxMap { //manages the aux maps
 
         boolean file1Exists = false;
         boolean file2Exists = false;
-        try
-        {
+        try {
             return file1.exists() && file2.exists();
-        } catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
         return false;
     }
 
-    public static void serializeTable(Set<MatchFieldEnum> attrs, List<Map<Long, Long>> pair){
+    public static void serializeTable(Set<MatchFieldEnum> attrs, List<Map<Long, Long>> pair) {
         //serialize hashmap locally
         Map<Long, Long> auxTable_idToAttr = pair.get(0);
         Map<Long, Long> auxTable_attrToId = pair.get(1);
@@ -42,8 +39,7 @@ public class AuxMap { //manages the aux maps
         String field2idName = "data/" + attributes_str + "_to_id.ser";
 
 
-        try
-        {
+        try {
             FileOutputStream fos = new FileOutputStream(id2fieldName);
             ObjectOutputStream oos = new ObjectOutputStream(fos);
             oos.writeObject(auxTable_idToAttr);
@@ -57,14 +53,13 @@ public class AuxMap { //manages the aux maps
             oos2.close();
             fos2.close();
             System.out.printf("Serialized HashMap data is saved in data/fields2id.ser");
-        }catch(IOException ioe)
-        {
+        } catch (IOException ioe) {
             ioe.printStackTrace();
         }
     }
 
 
-    public static List<Map<Long, Long>> deserializeTables(Set<MatchFieldEnum> attrs){
+    public static List<Map<Long, Long>> deserializeTables(Set<MatchFieldEnum> attrs) {
         List<MatchFieldEnum> attrList = new ArrayList<MatchFieldEnum>(attrs);
         Collections.sort(attrList);
         String attributes_str = "";
@@ -77,52 +72,44 @@ public class AuxMap { //manages the aux maps
         HashMap<Long, Long> id2field = null;
         HashMap<Long, Long> field2id = null;
 
-        try
-        {
+        try {
             FileInputStream fis = new FileInputStream(id2fieldName);
             ObjectInputStream ois = new ObjectInputStream(fis);
             id2field = (HashMap) ois.readObject();
             ois.close();
             fis.close();
-        }catch(IOException ioe)
-        {
+        } catch (IOException ioe) {
             ioe.printStackTrace();
             return null;
-        }catch(ClassNotFoundException c)
-        {
+        } catch (ClassNotFoundException c) {
             System.out.println("Class not found");
             c.printStackTrace();
             return null;
         }
 
-        try
-        {
+        try {
             FileInputStream fis = new FileInputStream(field2idName);
             ObjectInputStream ois = new ObjectInputStream(fis);
             field2id = (HashMap) ois.readObject();
             ois.close();
             fis.close();
-        }catch(IOException ioe)
-        {
+        } catch (IOException ioe) {
             ioe.printStackTrace();
             return null;
-        }catch(ClassNotFoundException c)
-        {
+        } catch (ClassNotFoundException c) {
             System.out.println("Class not found");
             c.printStackTrace();
             return null;
         }
 
-        if (id2field != null && field2id != null){
+        if (id2field != null && field2id != null) {
             return List.of(id2field, field2id);
         }
 
         return null;
-    };
+    }
 
-
-
-    public static String calculateAttrStr(Set<MatchFieldEnum> attrs){
+    public static String calculateAttrStr(Set<MatchFieldEnum> attrs) {
         List<MatchFieldEnum> attrList = new ArrayList<MatchFieldEnum>(attrs);
         Collections.sort(attrList);
         String attributes_str = "";
