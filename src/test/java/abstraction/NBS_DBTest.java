16d82bc01bd4151e8ef78106b1f6394b3bc7351c
Anish <anlsh@protonmail.com>
2020-02-17 11:24:33 -0500

    Add timing statements to the code

--- src/test/java/abstraction/NBS_DBTest.java
+++ src/test/java/abstraction/NBS_DBTest.java
@@ -1,6 +1,7 @@
 package abstraction;
 
 import algorithm.Deduplication;
+import com.google.common.base.Stopwatch;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import hashing.HashUtils;
@@ -16,18 +17,26 @@ public class NBS_DBTest {
 
     private NBS_DB db;
     private Deduplication dService;
+    Stopwatch timer;
 
     @Before
     public void setupDatabaseConnection() throws SQLException {
         db = new NBS_DB(Constants.DB_SERVER, Constants.DB_PORT, Constants.DB_NAME,
                 Constants.DB_USERNAME, Constants.DB_PASSWORD);
         dService = new Deduplication();
+        timer = Stopwatch.createUnstarted();
     }
 
     @Test
     public void testCreateAuxMap() {
-        AuxMap aux = db.constructAuxMap(Sets.newHashSet(Lists.newArrayList(MatchFieldEnum.FIRST_NAME,
-                MatchFieldEnum.SSN)));
+        timer.start();
+        AuxMap aux = db.constructAuxMap(Sets.newHashSet(Lists.newArrayList(
+                MatchFieldEnum.FIRST_NAME,
+                MatchFieldEnum.SSN
+        )));
+        timer.stop();
+
+        System.out.println("Test finished in " + timer);
 
         assert (aux.hashToIdMap != null);
         assert (aux.idToHashMap != null);
@@ -36,19 +45,6 @@ public class NBS_DBTest {
         assert aux.idToHashMap.size() > 0;
     }
 
-
-//    @Test
-//    public void testGetOrCreateAuxTable() throws SQLException {
-//        dedupService.create_or_get_aux_db(this.db, Lists.newArrayList(Sets.newHashSet(Lists.newArrayList(MatchFieldEnum.FIRST_NAME, MatchFieldEnum.SSN)),
-//                Sets.newHashSet(Lists.newArrayList(MatchFieldEnum.FIRST_NAME, MatchFieldEnum.LAST_NAME))));
-//    }
-//
-//    @Test
-//    public void testGetDuplicates() throws Exception{
-//        dedupService.get_duplicates(db, Lists.newArrayList(Sets.newHashSet(Lists.newArrayList(MatchFieldEnum.FIRST_NAME, MatchFieldEnum.SSN)),
-//                Sets.newHashSet(Lists.newArrayList(MatchFieldEnum.FIRST_NAME, MatchFieldEnum.LAST_NAME))));
-//    }
-//
     @Test
     public void testAddHook() throws Exception{
         AuxMapManager.hookAddRecord(db, Sets.newHashSet(Lists.newArrayList(MatchFieldEnum.FIRST_NAME, MatchFieldEnum.SSN)), 100L, HashUtils.hash(0));
