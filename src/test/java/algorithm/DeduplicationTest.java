c696e37a73981e5f067191bd24c252b2b06ee187
Anish <anlsh@protonmail.com>
2020-02-17 11:24:33 -0500

    Add timing statements to the code

--- src/test/java/algorithm/DeduplicationTest.java
+++ src/test/java/algorithm/DeduplicationTest.java
@@ -2,6 +2,7 @@ package algorithm;
 
 import abstraction.MatchFieldEnum;
 import abstraction.NBS_DB;
+import com.google.common.base.Stopwatch;
 import com.google.common.collect.Sets;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,22 +17,31 @@ import static org.junit.Assert.*;
 public class DeduplicationTest {
     private NBS_DB db;
     private Deduplication dService;
+    Stopwatch timer;
 
     @Before
     public void setupDatabaseConnection() throws SQLException {
         db = new NBS_DB("localhost", 1433, "ODS_PRIMARY_DATA01",
                 "SA", "saYyWbfZT5ni7t");
         dService = new Deduplication();
+        timer = Stopwatch.createUnstarted();
     }
 
-    //TODO fix these tests
     @Test
-    public void testGetMatches() throws Exception{
+    public void testGetMatches() throws Exception {
 
         List<Set<MatchFieldEnum>> config = new ArrayList<>();
-        config.add(Sets.newHashSet(MatchFieldEnum.FIRST_NAME, MatchFieldEnum.LAST_NAME));
+        config.add(Sets.newHashSet(
+                MatchFieldEnum.FIRST_NAME,
+                MatchFieldEnum.LAST_NAME
+        ));
 
+
+        timer.start();
         List<Set<Set<Long>>> res = dService.getMatching(db, config);
+        timer.stop();
+
+        System.out.println("Test completed in " + timer);
         System.out.println(res);
     }
 
