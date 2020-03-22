c4dc89f27e2e61561290ca873162b6e26409d64f
Winston <wzli98@gmail.com>
2020-03-22 19:58:08 -0400

    synchronized methods

--- src/main/java/abstraction/AuxMapManager.java
+++ src/main/java/abstraction/AuxMapManager.java
@@ -3,16 +3,16 @@ package abstraction;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.google.common.hash.HashCode;
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class AuxMapManager {
 
     private static String DATA_ROOT = "/tmp/aux-maps/";
+    private static String AUXMAP_MANAGER = "/tmp/aux-maps/manager.map";
+
     public static String getDataRoot() { return DATA_ROOT; }
     // TODO Make the setter for this do proper validation on directory name
     public static void setDataRoot(String curr) { DATA_ROOT = curr; }
@@ -29,6 +29,16 @@ public class AuxMapManager {
         return attributes_str;
     }
 
+    private static Map<String, Set<MatchFieldEnum>> getOrCreateMapManager(){
+        File managerFile = new File(AUXMAP_MANAGER);
+        if (managerFile.exists()){
+            return loadManagerFromFile();
+        } else {
+            HashMap<String, Set<MatchFieldEnum>> manager = new HashMap<String, Set<MatchFieldEnum>>();
+            return manager;
+        }
+    }
+
     public static String mfieldSetToFilename(final Set<MatchFieldEnum> attrs) {
         return DATA_ROOT + mfieldSetToString(attrs) + ".auxmap";
     }
@@ -39,7 +49,7 @@ public class AuxMapManager {
         return auxMapFile.exists();
     }
 
-    public static void saveAuxMapToFile(AuxMap aux) {
+    public synchronized static void saveAuxMapToFile(AuxMap aux) {
 
         deleteAuxMap(aux.attrs);
 
@@ -53,6 +63,40 @@ public class AuxMapManager {
             ObjectOutputStream oos = new ObjectOutputStream(fos);
             oos.writeObject(aux);
             oos.close();
+        } catch (Exception e) {
+            // TODO Exception handling
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    public synchronized static void saveManagerToFile(Map<String, Set<MatchFieldEnum>> manager){
+        deleteAuxMapManager();
+
+        try {
+            File managerFile = new File(AUXMAP_MANAGER);
+            if (managerFile.getParentFile() != null){
+                managerFile.getParentFile().mkdirs();
+            }
+
+            FileOutputStream fos = new FileOutputStream(managerFile);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(manager);
+            oos.close();
+
+        } catch (Exception e){
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    public static Map<String, Set<MatchFieldEnum>> loadManagerFromFile(){
+        try {
+            FileInputStream fin = new FileInputStream(AUXMAP_MANAGER);
+            ObjectInputStream ois = new ObjectInputStream(fin);
+            HashMap<String, Set<MatchFieldEnum>> manager = (HashMap<String, Set<MatchFieldEnum>>) ois.readObject();
+            ois.close();
+            return manager;
         } catch (Exception e) {
             // TODO Exception handling
             e.printStackTrace();
@@ -79,6 +123,12 @@ public class AuxMapManager {
         File auxMapfile = new File(mfieldSetToFilename(attrs));
         auxMapfile.delete();
     }
+
+    public static void deleteAuxMapManager(){
+        File auxMapFile = new File(AUXMAP_MANAGER);
+        auxMapFile.delete();
+    }
+
     public static AuxMap getAuxMap(NBS_DB db, Set<MatchFieldEnum> attrs, boolean delete_existing) {
         if (delete_existing) {
             deleteAuxMap(attrs);
@@ -87,7 +137,10 @@ public class AuxMapManager {
             return loadAuxMapFromFile(attrs);
         } else {
             AuxMap aux = db.constructAuxMap(attrs);
+            Map<String, Set<MatchFieldEnum>> manager = getOrCreateMapManager();
             saveAuxMapToFile(aux);
+            manager.put(mfieldSetToFilename(attrs), attrs);
+            saveManagerToFile(manager);
             return aux;
         }
     }
