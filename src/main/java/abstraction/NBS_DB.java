8b099e83b2952d7e511335222f20a296ce12e232
Anish Moorthy <anlsh@protonmail.com>
2020-03-24 15:12:44 -0400

    Paralellize the initial creation of AuxMaps

--- src/main/java/abstraction/NBS_DB.java
+++ src/main/java/abstraction/NBS_DB.java
@@ -2,9 +2,11 @@ package abstraction;
 
 import java.sql.*;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 import com.google.common.collect.Lists;
-import com.google.common.collect.Sets;
 import com.google.common.hash.HashCode;
 import hashing.HashUtils;
 
@@ -59,7 +61,7 @@ public class NBS_DB {
                 for(String reqiredColumn : mfield.getRequiredColumnsArray()) {
                     currTableColumns.add(
                             MatchFieldUtils.getSQLQualifiedColName(tableName, reqiredColumn)
-                            + MatchFieldUtils.getAliasedColName(tableName, reqiredColumn)
+                            + " as " + MatchFieldUtils.getAliasedColName(tableName, reqiredColumn)
                     );
                 }
             }
@@ -96,6 +98,7 @@ public class NBS_DB {
      */
     public AuxMap constructAuxMap(final Set<MatchFieldEnum> attrs, int num_threads) {
 
+        // Obtain a ResultSet object through which to access the database
         ResultSet rs;
         String queryString = getSQLQueryForAllEntries(attrs, null);
 
@@ -107,11 +110,48 @@ public class NBS_DB {
             throw new RuntimeException("Could not connect to and query SQL database");
         }
 
-        Map<Long, HashCode> idToHash = new HashMap<>();
-        Map<HashCode, Set<Long>> hashToIDs = new HashMap<>();
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(num_threads);
+        Map<Long, HashCode> idToHash = new ConcurrentHashMap<>();
+        // The sets referred to in the type signature below must of course be synchronized. However, Java does not seem
+        // to have a "concurrent set" type, although they can in fact be created by "deriving" from a Hash Map.
+        // We take that approach below https://better-coding.com/solved-how-to-create-concurrenthashset-before-and-after-java-8/
+        Map<HashCode, Set<Long>> hashToIDs = new ConcurrentHashMap<>();
 
-        List<Map<MatchFieldEnum, Object>> dbD= new LinkedList<>();
+        class HashDatabaseEntry implements Runnable {
 
+            /**
+             * Represents a job which will calculate the hash of the given attribute map and update the relevant
+             * maps for the final AuxMap
+             */
+
+            Map<MatchFieldEnum, Object> attr_map;
+            long uid;
+            HashDatabaseEntry(long uid, Map<MatchFieldEnum, Object> attr_map) {
+                this.uid = uid;
+                this.attr_map = attr_map;
+            }
+
+            @Override
+            public void run() {
+                HashCode hash = HashUtils.hashFields(attr_map);
+                idToHash.put(uid, hash);
+
+                Set<Long> idsWithSameHash = hashToIDs.getOrDefault(hash, null);
+                if (idsWithSameHash != null) {
+                    idsWithSameHash.add(uid);
+                } else {
+                    Map<Long, Boolean> myMap = new ConcurrentHashMap<>();
+                    Set<Long> concurrentSet = Collections.newSetFromMap(myMap);
+                    concurrentSet.add(uid);
+                    hashToIDs.put(hash, concurrentSet);
+                }
+            }
+        }
+
+        // Loop over the items in the ResultSet, submitting them to a thread pool to be hashed in a concurrent fashion.
+        // TODO In the case that jobs are added to the queue faster than they can be processed, it is possible that
+        // calls to the ThreadPoolExecutor's execute function should block until there are less than <n> items in
+        // its job queue. Otherwise, this function may essentially load the entire database into RAM
         try {
             while (rs.next()) {
                 Map<MatchFieldEnum, Object> attr_map = new HashMap<>();
@@ -129,18 +169,10 @@ public class NBS_DB {
 
                 if (include_entry) {
                     long record_id = (long) MatchFieldEnum.UID.getFieldValue(rs);
-                    HashCode hash = HashUtils.hashFields(attr_map);
-
-                    idToHash.put(record_id, hash);
-
-                    Set<Long> idsWithSameHash = hashToIDs.getOrDefault(hash, null);
-                    if (idsWithSameHash != null) {
-                        idsWithSameHash.add(record_id);
-                    } else {
-                        hashToIDs.put(hash, Sets.newHashSet(record_id));
-                    }
+                    executor.execute(new HashDatabaseEntry(record_id, attr_map));
                 }
             }
+            executor.shutdown();
         } catch (SQLException e) {
             // TODO Exception Handling
             e.printStackTrace();
@@ -150,6 +182,7 @@ public class NBS_DB {
         return new AuxMap(attrs, idToHash, hashToIDs);
     }
     public AuxMap constructAuxMap(final Set<MatchFieldEnum> attrs) {
-        return constructAuxMap(attrs, 1);
+        // TODO MAGIC NUMBER
+        return constructAuxMap(attrs, Constants.NUM_AUXMAP_THREADS);
     }
 }
