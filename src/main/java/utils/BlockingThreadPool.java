4e832dabd646d50f66fe103556659e12908dd746
Anish Moorthy <anlsh@protonmail.com>
2020-04-13 14:12:30 -0400

    Whitespace police

--- src/main/java/utils/BlockingThreadPool.java
+++ src/main/java/utils/BlockingThreadPool.java
@@ -7,16 +7,12 @@ import java.util.concurrent.TimeUnit;
 // See https://stackoverflow.com/questions/4521983/java-executorservice-that-blocks-on-submission-after-a-certain-queue-size
 // for very important information about this class and its functionality
 
-class LimitedQueue<E> extends LinkedBlockingQueue<E>
-{
-    public LimitedQueue(int maxSize)
-    {
+class LimitedQueue<E> extends LinkedBlockingQueue<E> {
+    public LimitedQueue(int maxSize) {
         super(maxSize);
     }
-
     @Override
-    public boolean offer(E e)
-    {
+    public boolean offer(E e) {
         // turn offer() and add() into a blocking calls (unless interrupted)
         try {
             put(e);
@@ -26,10 +22,10 @@ class LimitedQueue<E> extends LinkedBlockingQueue<E>
         }
         return false;
     }
-
 }
 
 public class BlockingThreadPool extends ThreadPoolExecutor {
+
     public BlockingThreadPool(int num_threads, int queue_capacity) {
         super(num_threads, num_threads, 2000, TimeUnit.MILLISECONDS, new LimitedQueue<>(queue_capacity));
     }
