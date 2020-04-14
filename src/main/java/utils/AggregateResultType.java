0f047c54746bd2caae587284f6317ec6d158baec
Anish Moorthy <anlsh@protonmail.com>
2020-04-14 15:14:40 -0400

    Document AggregateResultType

--- src/main/java/utils/AggregateResultType.java
+++ src/main/java/utils/AggregateResultType.java
@@ -10,30 +10,40 @@ import java.util.Map;
 import java.util.Set;
 
 /**
- * TODO Document
+ * Encapsulating object for
+ *      (a) values of a set of MatchFieldEnum on a specific record and
+ *      (b) whether there are any unknown values in (a)
  */
 public class AggregateResultType {
     private Map<MatchFieldEnum, Object> values;
     private boolean isUnknown;
 
+    /** Given a set of MatchFieldEnums and a record, extract values for the match fields.
+     *
+     * The set of extracted values is guaranteed to include all requested values only when all of them are known.
+     * Otherwise, all extracted values are discarded.
+     * @param attrs     A set of MatchFieldEnum whose values are to be extracted
+     * @param rs        A ResultSet whose current active row is the record from which to extract *attrs*
+     */
     public AggregateResultType(Set<MatchFieldEnum> attrs, ResultSet rs) {
 
-        values = new HashMap<>();
-        isUnknown = false;
+        Map<MatchFieldEnum, Object> vmap = new HashMap<>();
+        isUnknown = true;
 
         try {
             for (MatchFieldEnum mfield : new ArrayList<>(attrs)) {
                 ResultType result = mfield.getFieldValue(rs);
                 if (result.isUnknown()) {
-                    isUnknown = true;
-                    break;
+                    return;
                 } else {
-                    values.put(mfield, result.getValue());
+                    vmap.put(mfield, result.getValue());
                 }
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+        isUnknown = false;
+        values = vmap;
     }
 
     public Map<MatchFieldEnum, Object> getValues() { return values; }
